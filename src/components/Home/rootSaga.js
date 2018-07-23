150791ad4e764bd728b931060d94cf88a83bb59b
zhaoweinan <zhaoweinan@juyike.cn>
2018-07-23 17:02:24 +0800

    remove commons

--- src/components/Home/rootSaga.js
+++ src/components/Home/rootSaga.js
@@ -6,7 +6,6 @@ export function* sagaGetData(){
     try{
       const list = yield fetch('./api/articles.json')
       .then(response => response.json());
-      console.log(list,"@@@@@")
      yield put({type:"LOAD_ARTICLES_SUCCESS",payload:{articleList: list}})
     }catch(err){
       yield  put({type:"LOAD_ARTICLES_ERROR",err})
